9043a4edf854bde9a7fee1d6df2fafa1216c8a14
yi.zhu <yi.zhu@liulishuo.com>
2019-10-14 13:41:53 +0800

    feat(setup): Run PlugInstall Automaticly

--- setup_for_macos.sh
+++ setup_for_macos.sh
@@ -1,6 +1,16 @@
 # brew
 which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# oh-my-zsh
+which zsh || sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -sf `pwd`/self.zsh-theme ~/.oh-my-zsh/themes/self.zsh-theme
+ln -sf `pwd`/zshrc ~/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze && ~/.scm_breeze/install.sh
+brew install autojump &> /dev/null
+echo "Successfully deploy zsh config"
+
 # gotty
 ln -sf `pwd`/gotty ~/.gotty
 
@@ -11,8 +21,8 @@ which gotags || brew install gotags
 mkdir ~/.config/nvim/
 ln -sf `pwd`/vimrc ~/.config/nvim/init.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +q +q
 echo "Successfully deploy neovim config"
-echo "You can run :PlugInstall in your vim to install all plugins"
 
 # tmux
 which tmux || brew install tmux
@@ -26,17 +36,6 @@ npm install -g lint-staged husky
 ln -sf `pwd`/czrc ~/.czrc
 echo "Successfully install related tools for git"
 echo "You can edit your package.json to enable lint-staged or other commit linter"
-
-
-# oh-my-zsh
-which zsh || sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -sf `pwd`/self.zsh-theme ~/.oh-my-zsh/themes/self.zsh-theme
-ln -sf `pwd`/zshrc ~/.zshrc
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze && ~/.scm_breeze/install.sh
-brew install autojump &> /dev/null
-echo "Successfully deploy zsh config"
 
 # gsed
 which gsed || brew install gnu-sed
